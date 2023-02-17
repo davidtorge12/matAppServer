289f3241ab1699abb109d9304d73952b28432dc6
David Torge <david.torge@creatopy.com>
2023-02-18 00:10:57 +0200

    update env

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors());
 const port = process.env.PROT || 3000;
 
-mongoose.connect(process.env.MONGO_DB_URL);
+mongoose.connect("mongodb+srv://matUser:matUser123@matapp.5r9058l.mongodb.net/matapp");
 
 app.use(express.json());
 
